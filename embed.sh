5749d81df5058866002ea932d1d5d76414dea855
Renaud Guillard <dev@nore.fr>
2015-10-13 21:18:47 +0200

    update ns_mktemp function

--- embed.sh
+++ embed.sh
@@ -22,22 +22,27 @@ EOF
 # Create a temporary file
 ns_mktemp()
 {
-		local key=
-		if [ $# -gt 0 ]
-		then
-				key="${1}"
-				shift
-		else
-				key="$(date +%s)"
-		fi
-		if [ "$(uname -s)" == "Darwin" ]
-		then
-				#Use key as a prefix
-				mktemp -t "${key}"
-		else
-				#Use key as a suffix
-				mktemp --suffix "${key}"
-		fi
+	local key=
+	if [ $# -gt 0 ]
+	then
+		key="${1}"
+		shift
+	else
+		key="$(date +%s)"
+	fi
+	local __ns_mktemp=
+	if [ "$(uname -s)" == "Darwin" ]
+	then
+		#Use key as a prefix
+		mktemp -t "${key}"
+	elif which 'mktemp' 1>/dev/null 2>&1
+	then
+		# Use key as a suffix
+		mktemp --suffix "${key}"
+	else
+	local __ns_mktemp="/tmp/${key}.$(date +%s)-${RANDOM}"
+	touch "${__ns_mktemp}" && echo "${__ns_mktemp}"
+	fi
 }
 
 # Get real absolute path of a file or directory
